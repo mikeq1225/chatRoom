511ab6d747f59028b93f1b9bf3b86c7d9f46a8a4
Michael Quarne <mikedq1225@gmail.com>
2020-03-13 10:56:56 -0700

    finished MVP - no styling closes #2 #3

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,36 +1,12 @@
-import React, { useState } from "react"
-import io from "socket.io-client"
+import React from "react"
+import MessageForm from "./MessageForm"
+import Message from "./Message"
 
 export default props => {
-  const [text, setText] = useState("")
-  const [messages, setMessages] = useState([])
-
-  const socket = io.connect("http://10.255.255.11:3001")
-  socket.on("message", msg => {
-    setMessages([...messages, msg])
-  })
-
-  function handleClick(e) {
-    e.preventDefault()
-    socket.emit("message", text)
-    setText("")
-  }
-
   return (
     <div>
-      <div>
-        <input
-          type="text"
-          value={text}
-          onChange={e => setText(e.target.value)}
-        />
-        <button onClick={handleClick}>Send</button>
-      </div>
-      <div>
-        {messages.map((each, i) => (
-          <p key={"each" + i}>{each}</p>
-        ))}
-      </div>
+      <MessageForm />
+      <Message />
     </div>
   )
 }
