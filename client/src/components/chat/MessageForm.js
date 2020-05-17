d345d5e47d9434111970d352dc1caed15f5b6892
Michael Quarne <mikedq1225@gmail.com>
2020-05-17 12:22:29 -0700

    changed color scheme

--- client/src/components/chat/MessageForm.js
+++ client/src/components/chat/MessageForm.js
@@ -2,64 +2,64 @@ import React, { useState, useRef } from "react"
 import { useChat } from "../../hooks"
 import { useAuth } from "react-auth"
 
-export default props => {
-  const { add } = useChat()
-  const [message, setMessage] = useState("")
-  const { profile } = useAuth()
-  const [fontColor, setFontColor] = useState("#eeeeee")
-  const [fontSize, setFontSize] = useState("12")
-  const inputRef = useRef(null)
+export default (props) => {
+	const { add } = useChat()
+	const [message, setMessage] = useState("")
+	const { profile } = useAuth()
+	const [fontColor, setFontColor] = useState("#eeeeee")
+	const [fontSize, setFontSize] = useState("14")
+	const inputRef = useRef(null)
 
-  function handleSubmit(e) {
-    e.preventDefault()
-    add({
-      user: profile.username,
-      msg: message,
-      timestamp: new Date().getTime(),
-      fontColor: fontColor,
-      fontSize: fontSize
-    })
+	function handleSubmit(e) {
+		e.preventDefault()
+		add({
+			user: profile.username,
+			msg: message,
+			timestamp: new Date().getTime(),
+			fontColor: fontColor,
+			fontSize: fontSize,
+		})
 
-    setMessage("")
-    inputRef.current.focus()
-  }
+		setMessage("")
+		inputRef.current.focus()
+	}
 
-  function keyUp(e) {
-    if (e.key === "Enter") {
-      handleSubmit(e)
-    }
-  }
+	function keyUp(e) {
+		if (e.key === "Enter") {
+			handleSubmit(e)
+		}
+	}
 
-  return (
-    <div className="footer">
-      <form onSubmit={handleSubmit}>
-        <div className="inputDiv">
-          <input
-            style={{ color: fontColor, fontSize: fontSize + "px" }}
-            className="messageText"
-            value={message}
-            type="text"
-            onChange={e => setMessage(e.target.value)}
-            placeholder={"Send a message to " + props.room + "..."}
-            onKeyUp={keyUp}
-            ref={inputRef}
-          />
-          <div>
-            <input
-              className="messageColor"
-              type="color"
-              value={fontColor}
-              onChange={e => setFontColor(e.target.value)}
-            />
-            <input
-              className="messageSize"
-              type="text"
-              value={fontSize}
-              onChange={e => setFontSize(e.target.value)}
-            />
-          </div>
-        </div>
-      </form>
-    </div>
-  )
+	return (
+		<div className="footer">
+			<form onSubmit={handleSubmit}>
+				<div className="inputDiv">
+					<input
+						style={{ color: fontColor, fontSize: fontSize + "px" }}
+						className="messageText"
+						value={message}
+						type="text"
+						onChange={(e) => setMessage(e.target.value)}
+						placeholder={"Send a message to " + props.room + "..."}
+						onKeyUp={keyUp}
+						ref={inputRef}
+					/>
+					<div>
+						<input
+							className="messageColor"
+							type="color"
+							value={fontColor}
+							onChange={(e) => setFontColor(e.target.value)}
+						/>
+						<input
+							className="messageSize"
+							type="text"
+							value={fontSize}
+							onChange={(e) => setFontSize(e.target.value)}
+						/>
+					</div>
+				</div>
+			</form>
+		</div>
+	)
 }
--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client"
 
-// const ip = "http://10.255.255.11:3001"
+// const ip = "http://192.168.1.143:3001"
 const ip = "http://localhost:3001"
 
 const socket = io.connect(ip)
